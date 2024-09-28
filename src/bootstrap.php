6d9e3e2fa951cd7448c949c8ca11c0314460b3b5
Daniel Schulz <faulolio@gmail.com>
2024-09-28 22:41:30 +0200

    build: update files

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,17 +1,16 @@
 <?php
 declare(strict_types = 1);
+namespace Slothsoft\Server\Farah;
 
 use Slothsoft\Core\ServerEnvironment;
 use Slothsoft\Farah\Dictionary;
 use Slothsoft\Farah\Kernel;
 use Slothsoft\Farah\Module\Module;
 
-@include __DIR__ . '/../../../global/slothsoft.core.php';
-@include __DIR__ . '/../../../global/slothsoft.core.xslt.php';
-@include __DIR__ . '/../../../global/slothsoft.core.dbms.php';
-@include __DIR__ . '/../../../global/slothsoft.farah.php';
-// @include __DIR__ . '/../../../global/slothsoft.minecraft.php';
-// @include __DIR__ . '/../../../global/slothsoft.mtg.php';
+// @include __DIR__ . '/../../global/slothsoft.core.php';
+// @include __DIR__ . '/../../global/slothsoft.core.xslt.php';
+// @include __DIR__ . '/../../global/slothsoft.core.dbms.php';
+// @include __DIR__ . '/../../global/slothsoft.farah.php';
 
 $root = dirname(__DIR__);
 
@@ -20,9 +19,8 @@ ServerEnvironment::setCacheDirectory($root . DIRECTORY_SEPARATOR . 'cache');
 ServerEnvironment::setLogDirectory($root . DIRECTORY_SEPARATOR . 'log');
 ServerEnvironment::setDataDirectory($root . DIRECTORY_SEPARATOR . 'data');
 
-Module::registerWithXmlManifestAndDefaultAssets('slothsoft@farah.slothsoft.net', $root . DIRECTORY_SEPARATOR . 'assets');
-
-Kernel::setCurrentSitemap('farah://slothsoft@farah.slothsoft.net/sitemap');
+Kernel::setCurrentSitemap('farah://slothsoft/farah.slothsoft.net/sitemap');
 Kernel::setTrackingEnabled(false);
 Dictionary::setSupportedLanguages('en-us');
 
+Module::registerWithXmlManifestAndDefaultAssets('slothsoft@farah.slothsoft.net', $root . DIRECTORY_SEPARATOR . 'assets');
