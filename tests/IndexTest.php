7ec6d15b0e9a8afaa7e22a0e5c5e1296cdd4e0f0
Daniel Schulz <faulolio@gmail.com>
2026-03-13 18:38:30 +0100

    build: update files

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types = 1);
-namespace Slothsoft\Server\Farah\Tests;
+namespace Slothsoft\Server\Farah;
 
 use Slothsoft\Farah\Http\MessageFactory;
 use Slothsoft\FarahTesting\Module\AbstractTestCase;
